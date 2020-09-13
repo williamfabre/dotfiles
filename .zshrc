35c9a4dd397e6fe157b4f1ac710ec068f23f0586
Samuel Prevost <samuel.prevost@pm.me>
2020-09-13 23:21:25 +0200

    Updated install_utils with shell linting tools

--- .zshrc
+++ .zshrc
@@ -11,11 +11,15 @@ HIST_STAMPS="yyyy-mm-dd" # All hail ISO 8601
 export HISTTIMEFORMAT="%y-%m-%d %T "
 
 plugins=(
-    z
-    git
-    sudo
-    battery
-    aws
+	z
+	git
+	sudo
+	battery
+	aws
+	poetry
+	shell-aws-autoprofile
+	zsh-autosuggestions
+	zsh-syntax-highlighting
 )
 
 source $ZSH/oh-my-zsh.sh
@@ -26,7 +30,6 @@ export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
 ## PATH EXPORTS
-#export PATH="/usr/local/opt/curl/bin:$PATH"
 export PATH="/usr/lib/postgresql/11/bin:$PATH"
 
 export PATH="$HOME/.pyenv/bin:$PATH"
@@ -35,6 +38,7 @@ export PATH="$HOME/.plenv/bin:$PATH"
 export PATH="$HOME/.jenv/bin:$PATH"
 
 export PATH="$HOME/.poetry/bin:$PATH"
+export PATH=$(go env GOPATH)/bin:$PATH
 
 ## ALIASES LOADING
 if [ -f "$HOME/.zsh_aliases" ]; then
@@ -43,54 +47,53 @@ fi
 
 ## MISC GLOBAL VARS
 export EDITOR='vim'
-export TERM='xterm-256color'    # Necessary when through SSH
+export TERM='xterm-256color' # Necessary when through SSH
 export GPG_TTY=$(tty)
-if command -v brew 2>&1 > /dev/null; then
-    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
-    export HOMEBREW_CASK_OPTS=--require-sha
-    # Disable auto update auto update completely when set to 1
-    export HOMEBREW_NO_AUTO_UPDATE=0
-    # Time before autoupdate, default:60 sec
-    export HOMEBREW_AUTO_UPDATE_SECS=259200 # = 3*24*3600 = 3 days 
+if command -v brew 2>&1 >/dev/null; then
+	export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+	export HOMEBREW_CASK_OPTS=--require-sha
+	# Disable auto update auto update completely when set to 1
+	export HOMEBREW_NO_AUTO_UPDATE=0
+	# Time before autoupdate, default:60 sec
+	export HOMEBREW_AUTO_UPDATE_SECS=259200 # = 3*24*3600 = 3 days
 fi
 ## MISC PROGS EVALS
-if command -v tilix 2>&1 > /dev/null && [ $TILIX_ID ] || [ $VTE_VERSION ]; then
-    # https://gnunn1.github.io/tilix-web/manual/vteconfig/
-    source /etc/profile.d/vte.sh
+if command -v tilix 2>&1 >/dev/null && [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+	# https://gnunn1.github.io/tilix-web/manual/vteconfig/
+	source /etc/profile.d/vte.sh
 fi
-if command -v thefuck 2>&1 > /dev/null; then
-    eval $(thefuck --alias)
+if command -v thefuck 2>&1 >/dev/null; then
+	eval $(thefuck --alias)
 fi
 # This *is* the nicest interface for managing scripting languages
-if command -v pyenv 2>&1 > /dev/null; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    eval "$(pyenv init - zsh)"
+if command -v pyenv 2>&1 >/dev/null; then
+	export PYENV_ROOT="$HOME/.pyenv"
+	eval "$(pyenv init - zsh)"
 fi
-if command -v rbenv 2>&1 > /dev/null; then
-    eval "$(rbenv init - zsh)"
+if command -v rbenv 2>&1 >/dev/null; then
+	eval "$(rbenv init - zsh)"
 fi
-if command -v plenv 2>&1 > /dev/null; then
-    eval "$(plenv init - zsh)"
+if command -v plenv 2>&1 >/dev/null; then
+	eval "$(plenv init - zsh)"
 fi
-if command -v jenv 2>&1 > /dev/null; then
-    export JENV_ROOT="/usr/local/opt/jenv"
-    eval "$(jenv init - zsh)"
+if command -v jenv 2>&1 >/dev/null; then
+	export JENV_ROOT="/usr/local/opt/jenv"
+	eval "$(jenv init - zsh)"
 fi
-
 ## MISC GLOBAL VARS (post evals)
 
-# Adds the currently used python exec's sitepackage path to the PYTHONPATH, necessarry for jedi-vim 
+# Adds the currently used python exec's sitepackage path to the PYTHONPATH, necessarry for jedi-vim
 py3_sitepackage=$(python3 -c "import site; print(site.getsitepackages()[0])")
 if [ -d $py3_sitepackage ]; then
-    export PYTHONPATH="$py3_sitepackage:$PYTHONPATH"
+	export PYTHONPATH="$py3_sitepackage:$PYTHONPATH"
 fi
-if command -v /usr/libexec/java_home 2>&1 > /dev/null; then
-    current_java_version=$(java -version 2>&1 | gsed -E '1!d; s/.*"(.*)"$/\1/g')
-    export JAVA_HOME=$(/usr/libexec/java_home -v $current_java_version)
+if command -v /usr/libexec/java_home 2>&1 >/dev/null; then
+	current_java_version=$(java -version 2>&1 | gsed -E '1!d; s/.*"(.*)"$/\1/g')
+	export JAVA_HOME=$(/usr/libexec/java_home -v $current_java_version)
 fi
-if command -v aws 2>&1 > /dev/null; then
-    export AWS_TEST_PROFILE="edfx-poc-euw1"
-    export SAM_CLI_TELEMETRY=0
+if command -v aws 2>&1 >/dev/null; then
+	export AWS_TEST_PROFILE="edfx-poc-euw1"
+	export SAM_CLI_TELEMETRY=0
 fi
 
 ## MOTD LOADING
--- install_utils.sh
+++ install_utils.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 
-whichOrFail(){
-    if ! which $1; then
-        echo "Program $1 not found !"
-        exit 1
-    else
-        which $1
-    fi
-}
+echo "Welcome to the automatic utility installer !"
 
 if [ -f "/etc/debian_version" ]; then
-    echo "Debian-like detected"
-    echo "Installing basic tools to install the rest"
-    sudo apt-get install --yes zsh wget curl gzip p7zip-full gpg tree
-    echo "Done"
-elif [ "$(grep -Ei 'fedora|redhat' /etc/*release)" ]; then
-    echo "RHEL-like detected"
-    echo "Installing basic tools to install the rest"
-    sudo yum install --yes zsh wget curl gzip p7zip-full gpg tree
-    echo "Done"
+	echo "Debian-like detected"
+	echo "Installing basic tools to install the rest"
+	sudo apt-get install --yes zsh wget curl gzip p7zip-full gpg tree neofetch
+	echo "Done"
+elif grep -q -Ei 'fedora|redhat' /etc/*release; then
+	echo "RHEL-like detected"
+	echo "Installing basic tools to install the rest"
+	sudo yum install --yes zsh wget curl gzip p7zip-full gpg tree neofetch
+	echo "Done"
 else
-    echo "Distro not found, please specify a packet manager"
-    exit 1
+	echo "Distro not found, please specify a packet manager"
+	exit 1
 fi
 
 echo "This needs to run sudo many times for installs"
 sudo echo "Sudo unlocked successfuly"
 
-
+echo "#####################################################"
 echo "Installing oh-my-zsh using the automatic installer:"
 echo -e "\tBackuping original .zshrc"
 mv $HOME/.zshrc $HOME/.zshrc_while_installing_ohmyzsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv $HOME/.zshrc_while_installing_ohmyzsh $HOME/.zshrc 
+mv $HOME/.zshrc_while_installing_ohmyzsh $HOME/.zshrc
 
+echo "#####################################################"
+echo -n "Changing default shell to zsh... "
+sudo chsh -s $(which zsh) $USER
+echo "Done"
+
+echo "#####################################################"
 echo "Installing pyenv using the automatic pyenv installer:"
 curl https://pyenv.run | bash
 pyenv --version
 
+echo "#####################################################"
 echo -n "Installing tldr (go client)... "
 wget -O /tmp/tldr.tar.gz "https://github.com/pranavraja/tldr/releases/download/v1/tldr_linux_amd64.tar.gz" -q
 tar -xzf /tmp/tldr.tar.gz -C /tmp
@@ -46,7 +46,8 @@ rm -rf /tmp/tldr.tar.gz /tmp/tldr_linux_amd64
 echo "Done"
 tldr
 
-echo -n "Installing fdfind... "
+echo "#####################################################"
+echo -n "Installing fdfind v8.1.1... "
 wget -O /tmp/fd.tar.gz "https://github.com/sharkdp/fd/releases/download/v8.1.1/fd-v8.1.1-x86_64-unknown-linux-musl.tar.gz" -q
 tar -xzf /tmp/fd.tar.gz -C /tmp
 sudo mv /tmp/fd-v8.1.1-x86_64-unknown-linux-musl/fd /usr/bin/fd
@@ -54,15 +55,17 @@ rm -rf /tmp/fd.tar.gz /tmp/fd-v8.1.1-x86_64-unknown-linux-musl
 echo "Done"
 fd --version
 
-echo -n "Installing exa... "
+echo "#####################################################"
+echo -n "Installing exa v0.9.0... "
 wget -O /tmp/exa.zip "https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip" -q
-unzip /tmp/exa.zip -d /tmp > /dev/null 2>&1
+unzip /tmp/exa.zip -d /tmp >/dev/null 2>&1
 sudo mv /tmp/exa-linux-x86_64 /usr/bin/exa
 rm -rf /tmp/exa.zip
 echo "Done"
 exa --version
 
-echo -n "Installing bat... "
+echo "#####################################################"
+echo -n "Installing bat v0.15.4... "
 wget -O /tmp/bat.tar.gz "https://github.com/sharkdp/bat/releases/download/v0.15.4/bat-v0.15.4-x86_64-unknown-linux-gnu.tar.gz" -q
 tar -xzf /tmp/bat.tar.gz -C /tmp
 sudo mv /tmp/bat-v0.15.4-x86_64-unknown-linux-gnu/bat /usr/bin/bat
@@ -70,7 +73,8 @@ rm -rf /tmp/bat.tar.gz /tmp/bat-v0.15.4-x86_64-unknown-linux-gnu
 echo "Done"
 bat --version
 
-echo -n "Installing ripgrep... "
+echo "#####################################################"
+echo -n "Installing ripgrep v12.1.1... "
 wget -O /tmp/ripgrep.tar.gz "https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep-12.1.1-x86_64-unknown-linux-musl.tar.gz" -q
 tar -xzf /tmp/ripgrep.tar.gz -C /tmp
 sudo mv /tmp/ripgrep-12.1.1-x86_64-unknown-linux-musl/rg /usr/bin/rg
@@ -78,11 +82,30 @@ rm -rf /tmp/ripgrep.tar.gz /tmp/ripgrep-12.1.1-x86_64-unknown-linux-musl
 echo "Done"
 rg --version
 
-echo -n "Installing python libs... "
+echo "#####################################################"
+echo -n "Installing shfmt v3.1.2... "
+wget -O /tmp/shfmt "https://github.com/mvdan/sh/releases/download/v3.1.2/shfmt_v3.1.2_linux_amd64" -q
+sudo mv /tmp/shfmt /usr/bin/shfmt
+echo "Done"
+echo "shfmt $(shfmt --version)"
+
+echo "#####################################################"
+echo -n "Installing shellcheck v0.7.1... "
+wget -O /tmp/shellcheck.tar.xz "https://github.com/koalaman/shellcheck/releases/download/v0.7.1/shellcheck-v0.7.1.linux.x86_64.tar.xz" -q
+tar -xf /tmp/shellcheck.tar.xz -C /tmp
+sudo mv /tmp/shellcheck-v0.7.1/shellcheck /usr/bin/shellcheck
+rm -rf /tmp/shellcheck.tar.xz /tmp/shellcheck-v0.7.1
+echo "Done"
+shellcheck --version
+
+echo "#####################################################"
+echo "Upgrading pip... "
+pip3 install -U pip3
+
+echo "#####################################################"
+echo "Installing python libs... "
 pip3 install -U numpy Pillow scikit-image scikit-learn pandas opencv-python opencv-contrib-python black h5py ipywidgets jupyter jupyterlab tqdm requests poetry flask jsonschema beautifulsoup4 ipaddress urllib3 paramiko matplotlib tensorflow torch spacy statsmodels numpydoc keras
 echo "Done"
 
+echo "#####################################################"
 echo "Everything done installing"
-echo -n "Changing shell to zsh... "
-sudo chsh -s $(which zsh) $USER
-echo "Done"
